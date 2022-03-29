b3920168edeec2054da0c77d573f68e6fd2e2d33
Phillip Kruger <phillip.kruger@gmail.com>
2022-03-29 14:16:20 +1100

    Added better integration test for lit component
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- integration-tests/src/main/ui-lit/src/simple-greeting.js
+++ integration-tests/src/main/ui-lit/src/simple-greeting.js
@@ -1,19 +1,23 @@
 import {html, css, LitElement} from 'lit';
 
 export class SimpleGreeting extends LitElement {
-  static styles = css`p { color: blue }`;
+    static styles = css`p { color: blue }`;
 
-  static properties = {
-    name: {type: String},
-  };
+    static properties = {
+        name: {type: String},
+    };
 
-  constructor() {
-    super();
-    this.name = 'Somebody';
-  }
+    constructor() {
+        super();
+        this.name = 'Somebody';
+    }
 
-  render() {
-    return html`<p>Hello, ${this.name}!</p>`;
-  }
+    render() {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", "/api/quinoa", false );
+        xmlHttp.send( null );
+        const response = xmlHttp.responseText;
+        return html`<p class="greeting">${response} and ${this.name}</p>`;
+    }
 }
 customElements.define('simple-greeting', SimpleGreeting);
--- integration-tests/src/test/java/io/quarkiverse/quinoa/it/QuinoaUILitTest.java
+++ integration-tests/src/test/java/io/quarkiverse/quinoa/it/QuinoaUILitTest.java
@@ -1,10 +1,19 @@
 package io.quarkiverse.quinoa.it;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
+import java.net.URL;
+import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Response;
+
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 
@@ -12,17 +21,31 @@ import io.quarkus.test.junit.TestProfile;
 @TestProfile(QuinoaTestProfiles.LitTests.class)
 public class QuinoaUILitTest {
 
+    @TestHTTPResource("index.html")
+    URL url;
+
     @Test
     public void testUIIndex() {
-        given()
-                .when().get("/")
-                .then()
-                .statusCode(200)
-                .body(containsString("<title>Quinoa Lit App</title>"));
-        given()
-                .when().get("/index.html")
-                .then()
-                .statusCode(200)
-                .body(containsString("<title>Quinoa Lit App</title>"));
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions()
+                            .setArgs(List.of("--headless", "--disable-gpu", "--no-sandbox")));
+
+            BrowserContext context = browser.newContext();
+
+            Page page = context.newPage();
+            Response response = page.navigate(url.toString());
+            Assertions.assertEquals("OK", response.statusText());
+
+            page.waitForLoadState();
+
+            String title = page.title();
+            Assertions.assertEquals("Quinoa Lit App", title);
+
+            // Make sure the component loaded and hits the backend
+            String greeting = page.innerText(".greeting");
+            Assertions.assertEquals("Hello Quinoa and World", greeting);
+        }
     }
 }
