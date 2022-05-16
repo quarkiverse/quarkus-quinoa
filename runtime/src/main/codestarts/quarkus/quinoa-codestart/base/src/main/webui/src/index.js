8d1c85cf32dcd826bd01eee48f5c9c6e225ae3fe
Andy Damevin <ia3andy@gmail.com>
2022-05-16 15:27:49 +0200

    Fix typo

--- runtime/src/main/codestarts/quarkus/quinoa-codestart/base/src/main/webui/src/index.js
+++ runtime/src/main/codestarts/quarkus/quinoa-codestart/base/src/main/webui/src/index.js
@@ -4,7 +4,7 @@ function component() {
     const element = document.createElement('h1');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['<b>Q</b>uarkus', '<b>UI</b>', '<b>NO</b>', 'h<b>A</b>ssle'], ' ');
+    element.innerHTML = _.join(['<b>Q</b>uarkus', '<b>UI</b>', '<b>NO</b>', 'h<b>A</b>ssles'], ' ');
 
     return element;
 }
