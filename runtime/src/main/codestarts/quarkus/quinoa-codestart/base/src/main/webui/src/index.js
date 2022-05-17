8ae682f3144061fc1601c17ba5c7d62cd9200cc3
Andy Damevin <ia3andy@gmail.com>
2022-05-17 17:47:09 +0200

    Create a funny script in the codestart

--- runtime/src/main/codestarts/quarkus/quinoa-codestart/base/src/main/webui/src/index.js
+++ runtime/src/main/codestarts/quarkus/quinoa-codestart/base/src/main/webui/src/index.js
@@ -1,12 +1,22 @@
-import _ from 'lodash';
+import theaterJS from 'theaterjs';
 
-function component() {
-    const element = document.createElement('h1');
+const theater = theaterJS();
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['<b>Q</b>uarkus', '<b>UI</b>', '<b>NO</b>', 'h<b>A</b>ssles'], ' ');
+theater
+    .on('type:start, erase:start', function() { theater.getCurrentActor().$element.classList.add('actor-content--typing');})
+    .on('type:end, erase:end', function() {theater.getCurrentActor().$element.classList.remove('actor-content--typing');});
 
-    return element;
-}
-
-document.body.appendChild(component());
+theater
+    .addActor('Quarkus', { speed: 1, accuracy: 0.7 })
+    .addActor('Me', { speed: 0.9, accuracy: 0.8 })
+    .addScene('Quarkus:Toc toc.', 1000)
+    .addScene('Me:What?', 500)
+    .addScene('Quarkus:You will eat Quinoa today!', 200)
+    .addScene('Me:Nooo...', -3, '!!! ', 150, 'No! ', 150)
+    .addScene('Me:Yuk! That\'s impossible!', 100)
+    .addScene('Quarkus:It is time!', 100)
+    .addScene('Quarkus:With your training and this power,', 100)
+    .addScene('Quarkus:You will create awesome web apps.', 100)
+    .addScene('Quarkus:It is your destiny!', 200)
+    .addScene('Quarkus:Meet Quarkus UI with NO hAssles!', 200)
+    .addScene('Me:Neat!', 200)
