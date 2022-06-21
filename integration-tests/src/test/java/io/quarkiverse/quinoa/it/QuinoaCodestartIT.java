6f50931edcfb9ee0beb6977ec3f54f27cf985ffb
Andy Damevin <ia3andy@gmail.com>
2022-06-21 15:37:33 +0200

    Use setupStandaloneExtensionTest in QuinoaCodestartIT (#118)

--- integration-tests/src/test/java/io/quarkiverse/quinoa/it/QuinoaCodestartIT.java
+++ integration-tests/src/test/java/io/quarkiverse/quinoa/it/QuinoaCodestartIT.java
@@ -6,27 +6,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.devtools.testing.codestarts.QuarkusCodestartTest;
-import io.quarkus.maven.ArtifactCoords;
 
 public class QuinoaCodestartIT {
 
     @RegisterExtension
     public static QuarkusCodestartTest codestartTest = QuarkusCodestartTest.builder()
-            .standaloneExtensionCatalog()
-            .extension(
-                    ArtifactCoords.fromString("io.quarkiverse.quinoa:quarkus-quinoa:" + System.getProperty("project.version")))
+            .setupStandaloneExtensionTest("io.quarkiverse.quinoa:quarkus-quinoa")
             .languages(JAVA)
             .build();
 
-    static {
-        if (!System.getProperties().containsKey("project.version")) {
-            throw new IllegalStateException("project.version property is required");
-        }
-    }
-
     @Test
     void testContent() throws Throwable {
-        codestartTest.assertThatGeneratedFile(JAVA, "src/main/webui/package.json");
+        codestartTest.assertThatGeneratedFile(JAVA, "src/main/webui/package.json")
+                .exists();
         codestartTest.assertThatGeneratedTreeMatchSnapshots(JAVA, "src/main/webui");
     }
 
