ece797299c414233d4d6acbad703824e4e4d25c2
Melloware <mellowaredev@gmail.com>
2023-04-03 09:40:12 -0400

    DevUI: Logging and Install Button (#292)
    * DevUI: Logging and Install
    
    * DevUI: Logging and Install

---
+++ runtime/src/main/java/io/quarkiverse/quinoa/devui/QuinoaJsonRpcService.java
@@ -0,0 +1,41 @@
+package io.quarkiverse.quinoa.devui;
+
+import java.util.Collections;
+import java.util.Map;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.jboss.logging.Logger;
+
+import io.quarkus.dev.console.DevConsoleManager;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+
+@ApplicationScoped
+public class QuinoaJsonRpcService {
+
+    private static final Logger LOG = Logger.getLogger(QuinoaJsonRpcService.class);
+
+    @PostConstruct
+    void init() {
+        // no-op for now
+    }
+
+    public Multi<String> install() throws Exception {
+        LOG.info("Stopping Node server, installing new packages, and restarting...");
+        Map<String, String> params = Collections.EMPTY_MAP;
+
+        // For now, the JSON RPC are called on the event loop, but the action is blocking,
+        // So, work around this by invoking the action on a worker thread.
+        Multi<String> install = Uni.createFrom().item(() -> DevConsoleManager
+                .<String> invoke("quinoa-install-action", params))
+                .runSubscriptionOn(Infrastructure.getDefaultExecutor()) // It's a blocking action.
+                .toMulti();
+
+        return Multi.createBy().concatenating()
+                .streams(Multi.createFrom().item("started"), install);
+    }
+
+}
