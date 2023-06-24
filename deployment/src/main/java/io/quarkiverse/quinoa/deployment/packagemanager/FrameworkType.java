5bc77f85360d2c7d7d98fa23fe2ab7729a5e503d
Shivam Sharma <meshivam81@gmail.com>
2023-06-24 11:10:06 +0530

    SolidJS support

--- deployment/src/main/java/io/quarkiverse/quinoa/deployment/packagemanager/FrameworkType.java
+++ deployment/src/main/java/io/quarkiverse/quinoa/deployment/packagemanager/FrameworkType.java
@@ -28,6 +28,7 @@ public enum FrameworkType {
     REACT("build", "start", 3000, Set.of("react-scripts", "react-app-rewired", "craco")),
     VUE_LEGACY("dist", "serve", 3000, Set.of("vue-cli-service")),
     VITE("dist", "dev", 5173, Set.of("vite")),
+    SolidJS("dist", "dev", 3001, Set.of("vite")),
     NEXT("out", "dev", 3000, Set.of("next")),
     ANGULAR("dist/%s", "start", 4200, Set.of("ng")),
     WEB_COMPONENTS("dist", "start", 8003, Set.of("web-dev-server"));
