2da08f59dd7b96f632357c31b43bc104186f42b7
Andy Damevin <ia3andy@gmail.com>
2023-10-03 10:58:33 +0200

    Replace vue-legacy test by vue vite for stability (#537)

--- integration-tests/src/main/ui-vue/src/main.js
+++ integration-tests/src/main/ui-vue/src/main.js
@@ -1,4 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
