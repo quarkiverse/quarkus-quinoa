e2f7949bd12d5a632f7ce10f332e0fdfadab94e9
Yann Le Moigne <ylemoigne@javatic.fr>
2024-06-15 04:29:16 +0200

    Fix #682: Add tls support to real forwarder

--- deployment/src/main/java/io/quarkiverse/quinoa/deployment/items/ForwardedDevServerBuildItem.java
+++ deployment/src/main/java/io/quarkiverse/quinoa/deployment/items/ForwardedDevServerBuildItem.java
@@ -4,12 +4,24 @@ import io.quarkus.builder.item.SimpleBuildItem;
 
 public final class ForwardedDevServerBuildItem extends SimpleBuildItem {
 
+    private final boolean tls;
+    private final boolean tlsAllowInsecure;
     private final String host;
     private final Integer port;
 
-    public ForwardedDevServerBuildItem(String host, Integer port) {
+    public ForwardedDevServerBuildItem(boolean tls, boolean tlsAllowInsecure, String host, Integer port) {
+        this.tls = tls;
+        this.tlsAllowInsecure = tlsAllowInsecure;
         this.host = host;
         this.port = port;
+    }
+
+    public boolean isTls() {
+        return tls;
+    }
+
+    public boolean isTlsAllowInsecure() {
+        return tlsAllowInsecure;
     }
 
     public String getHost() {
