f60db46da92593c3c2227159a209ca4ac6e28e59
George Gastaldi <gegastaldi@gmail.com>
2026-04-09 09:45:19 -0300

    Add pre-hook for Quarkus Ecosystem CI workflow (#1011)
    - Ensure critical dependencies, including `pnpm`, `bun`, and Playwright browser tooling, are installed

---
+++ .github/workflows/hooks/pre-quarkus-ecosystem-ci.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#---------------------------------------------------
+# Hook invoked by the Quarkus Ecosystem CI workflow
+#---------------------------------------------------
+
+# Install pnpm
+npm install -g pnpm@~8.6.12
+
+# Install bun
+npm install -g bun
+
+# Ensure browsers are installed
+./current-repo/testing/../mvnw exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install-deps chromium"
